0cb0be3ddfa2c53c5067f27765631f67be530105
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-30 17:00:05 +0200

    M-3 event click on cells

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ const container = document.querySelector(`.little-container`);
 
 for (let i = 1; i < 101; i++) {
 	let square = document.createElement(`div`);
-	square.innerText = i;
 	square.classList.add(`square`);
 	container.append(square);
+
+	square.addEventListener(`click`, function () {
+		square.innerText = i;
+		square.classList.add(`bg-warning`);
+	});
 }
