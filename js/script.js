e2b16f6001788869470bd9b6bf6fba4c2a655d0b
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-04-30 19:05:36 +0200

    B-1 added play btn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(`.little-container`);
+const button = document.querySelector(`#btn`);
 
 for (let i = 1; i < 101; i++) {
 	let square = createClickableSquare(i);
 	container.append(square);
 }
+
+container.classList.add(`d-none`);
+
+button.addEventListener(`click`, function () {
+	container.classList.remove(`d-none`);
+	button.classList.add(`d-none`);
+});
