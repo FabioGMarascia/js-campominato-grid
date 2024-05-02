547243dfe27817aad45545ce90ec1afe119fe2c3
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-05-02 11:08:36 +0200

    useless iteration code

--- js/script.js
+++ js/script.js
@@ -7,29 +7,29 @@ select_value.value = `SELECT GAME MODE`;
 play_button.addEventListener(`click`, function () {
 	container.innerHTML = " ";
 	if (select_value.value == 1) {
+		container.classList.add(`easy-container`);
+		container.classList.remove(`medium-container`);
+		container.classList.remove(`hard-container`);
 		// ITERATION FOR EASY MODE
 		for (let i = 1; i < 101; i++) {
-			container.classList.add(`easy-container`);
-			container.classList.remove(`medium-container`);
-			container.classList.remove(`hard-container`);
 			let square = createClickableSquare(i);
 			container.append(square);
 		}
 	} else if (select_value.value == 2) {
+		container.classList.remove(`easy-container`);
+		container.classList.add(`medium-container`);
+		container.classList.remove(`hard-container`);
 		// ITERATION FOR MEDIUM MODE
 		for (let i = 1; i < 82; i++) {
-			container.classList.remove(`easy-container`);
-			container.classList.add(`medium-container`);
-			container.classList.remove(`hard-container`);
 			let square = createClickableSquare(i);
 			container.append(square);
 		}
 	} else if (select_value.value == 3) {
+		container.classList.remove(`easy-container`);
+		container.classList.remove(`medium-container`);
+		container.classList.add(`hard-container`);
 		// ITERATION FOR HARD MODE
 		for (let i = 1; i < 50; i++) {
-			container.classList.remove(`easy-container`);
-			container.classList.remove(`medium-container`);
-			container.classList.add(`hard-container`);
 			let square = createClickableSquare(i);
 			container.append(square);
 		}
